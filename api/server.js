e93296e48b0cf8ecc51997a856f778e1f5acaca4
Charlessnow <ekekwecharles50@gmail.com>
2024-10-16 08:57:07 +0100

    first commit

---
+++ api/server.js
@@ -0,0 +1,50 @@
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  const data = [
+    {
+      cityName: "Lisbon",
+      country: "Portugal",
+      emoji: "🇵🇹",
+      date: "2027-10-31T15:59:59.138Z",
+      notes: "My favorite city so far!",
+      position: {
+        lat: 38.727881642324164,
+        lng: -9.140900099907554,
+      },
+      id: 73930385,
+    },
+    {
+      cityName: "Madrid",
+      country: "Spain",
+      emoji: "🇪🇸",
+      date: "2027-07-15T08:22:53.976Z",
+      notes: "",
+      position: {
+        lat: 40.46635901755316,
+        lng: -3.7133789062500004,
+      },
+      id: 17806751,
+    },
+    {
+      cityName: "Berlin",
+      country: "Germany",
+      emoji: "🇩🇪",
+      date: "2027-02-12T09:24:11.863Z",
+      notes: "Amazing 😃",
+      position: {
+        lat: 52.53586782505711,
+        lng: 13.376933665713324,
+      },
+      id: 98443197,
+    },
+  ];
+
+  res.status(200).json(data);
+}
